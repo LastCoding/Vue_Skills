34cd8477633393dd24aa34c771ac812098d691b8
Nak <inyakimd@gmail.com>
2018-02-15 20:57:05 +0100

    Nav menu with Router

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
     <div id="app">
-        <Skills/>
+        <nav>
+            <router-link to="/">Home</router-link>
+            <router-link to="/about">About</router-link>
+        </nav>
+
+        <router-view />
     </div>
 </template>
 
@@ -24,7 +29,7 @@
         display: grid;
         grid-template-rows: auto;
         justify-items: center;
-        align-items: center;
+        padding-top: 50px;
     }
 
     body, html {
@@ -34,5 +39,19 @@
 
     #app {
         width: 50%;
+    }
+
+    nav {
+        padding: 20px 20px 20px 0;
+    }
+
+    nav a {
+        padding: 10px;
+        text-decoration: none;
+        background: #fff;
+        border-radius: 3px;
+        color: rgb(0, 110, 255);
+        font-weight: bold;
+        margin-right: 15px;
     }
 </style>
