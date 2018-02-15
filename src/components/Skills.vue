7884eb564d4e85e49a07df4b94637165941e3582
Nak <inyakimd@gmail.com>
2018-02-15 20:26:00 +0100

    Using third person libraries animation effects

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -5,7 +5,7 @@
             <form @submit.prevent="addSkill">
                 <input type="text" placeholder="Enter a skill you have.." v-model="skill" v-validate="'min:5'"
                        name="skill">
-                <transition name="alert-in">
+                <transition name="alert-in" enter-active-class="animated flipInX" leave-active-class="animated flipOutX">
                     <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
                 </transition>
             </form>
@@ -46,7 +46,8 @@
     }
 </script>
 
-<style>
+<style scoped>
+@import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
 
     .holder {
         background: #fff;
