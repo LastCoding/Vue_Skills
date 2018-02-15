effa6e336704a4c33127d3617c09f3977fea48b5
Nak <inyakimd@gmail.com>
2018-02-15 20:53:22 +0100

    Router Setup

---
+++ src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="about">
+        <h1>This is an about page</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ali...</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "about"
+    }
+</script>
+
+<style scoped>
+
+</style>
