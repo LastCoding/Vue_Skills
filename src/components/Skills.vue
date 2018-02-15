1b272181f620644741224ac4a75a21fe242d83bf
Nak <inyakimd@gmail.com>
2018-02-15 20:29:31 +0100

    Added transition bounce when adding skills

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -11,7 +11,9 @@
             </form>
 
             <ul>
+                <transition-group name="list" enter-active-class="animated bounceInUp" leave-active-class="animated bounceOutDown">
                 <li v-for="(data, index) in skills" :key="index">{{ data.skill }}</li>
+                </transition-group>
             </ul>
 
             <p>These are the skills that you possess.</p>
