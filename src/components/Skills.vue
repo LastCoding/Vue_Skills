a7b6ea69c48781cbd86f5220453460e98e796ae2
Nak <inyakimd@gmail.com>
2018-02-15 19:26:53 +0100

    Deleted css & some if/else

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -5,8 +5,6 @@
                 <li v-for="(data, index) in skills" :key="index">{{ index }}. {{ data.skill }}</li>
             </ul>
 
-            <p v-if="skills.length > 1">You have more than 1 skills</p>
-            <p v-else>You have less than or equal to 1 skill.</p>
         </div>
     </div>
 </template>
@@ -17,7 +15,8 @@
         data() {
             return {
                 skills: [
-                    { "skill": "Vue.js"}
+                    { "skill": "Vue.js"},
+                    { "skill": "Frotend Developer"}
                 ]
             }
         }
@@ -25,8 +24,6 @@
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style src="./Skills.css" scoped>
-body {
-    background-color: gray;
-}
+<style scoped>
+
 </style>
