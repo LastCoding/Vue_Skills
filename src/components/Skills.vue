53ce8d23d85532a5983b4203e91a5575d08e82c3
Nak <inyakimd@gmail.com>
2018-02-15 20:10:29 +0100

    Validate testing min characters

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -3,7 +3,8 @@
         <div class="holder">
 
             <form @submit.prevent="addSkill">
-                <input type="text" placeholder="Enter a skill you have.." v-model="skill">
+                <input type="text" placeholder="Enter a skill you have.." v-model="skill" v-validate="'min:5'" name="skill">
+                <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
             </form>
 
             <ul>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VeeValidate from 'vee-validate';
 
+Vue.use(VeeValidate);
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+    render: h => h(App)
 }).$mount('#app')
