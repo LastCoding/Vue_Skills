5d853bed436b86ea99b5d17198fd7f1a8a93825d
Nak <inyakimd@gmail.com>
2018-02-15 20:20:36 +0100

    Animation for validation text added

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -5,7 +5,9 @@
             <form @submit.prevent="addSkill">
                 <input type="text" placeholder="Enter a skill you have.." v-model="skill" v-validate="'min:5'"
                        name="skill">
-                <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
+                <transition name="alert-in">
+                    <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
+                </transition>
             </form>
 
             <ul>
@@ -90,5 +92,25 @@
         display: inline-block;
         padding: 5px;
         margin-top: -20px;
+    }
+
+    .alert-in-enter-active {
+        animation: bounce-in .5s;
+    }
+
+    .alert-in-leave-active {
+        animation: bounce-in .5s reverse;
+    }
+
+    @keyframes bounce-in {
+        0% {
+            transform: scale(0);
+        }
+        50% {
+            transform: scale(1.5);
+        }
+        100% {
+            transform: scale(1);
+        }
     }
 </style>
