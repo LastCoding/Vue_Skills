1b58c006bcdd9c32efa6aaadaab6d9f49e307a30
Nak <inyakimd@gmail.com>
2018-02-15 19:44:22 +0100

    Styling and getting rid of trash css code

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,38 @@
 <template>
-  <div id="app">
-    <Skills />
-  </div>
+    <div id="app">
+        <Skills/>
+    </div>
 </template>
 
 <script>
-import Skills from './components/Skills.vue'
+    import Skills from './components/Skills.vue'
 
-export default {
-  name: 'app',
-  components: {
-    Skills
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            Skills
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    @import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+
+    body {
+        background-color: #EEEEEE;
+        font-family: 'Montserrat', sans-serif;
+        display: grid;
+        grid-template-rows: auto;
+        justify-items: center;
+        align-items: center;
+    }
+
+    body, html {
+        margin: 0;
+        height: 100%;
+    }
+
+    #app {
+        width: 50%;
+    }
 </style>
--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="hello">
         <div class="holder">
+
             <ul>
-                <li v-for="(data, index) in skills" :key="index">{{ index }}. {{ data.skill }}</li>
+                <li v-for="(data, index) in skills" :key="index">{{ data.skill }}</li>
             </ul>
 
-            <div v-bind:style="{ backgroundColor: bgColor, width: bgWidth, height: bgHeight}"></div>
+            <p>These are the skills that you possess.</p>
         </div>
     </div>
 </template>
@@ -27,16 +28,35 @@
     }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-    .alert {
-        background-color: yellow;
-        width: 100%;
-        height: 30px;
+<style>
+
+    .holder {
+        background: #fff;
     }
 
-    .another-class {
-        border: 5px solid black;
+    ul {
+        margin: 0;
+        padding: 0;
+        list-style-type: none;
+    }
+
+    ul li {
+        padding: 20px;
+        font-size: 1.3em;
+        background-color: #E0EDF4;
+        border-left: 5px solid #3EB3F6;
+        margin-bottom: 2px;
+        color: #3E5252;
+    }
+
+    p {
+        text-align: center;
+        padding: 30px 0;
+        color: gray;
+    }
+
+    .container {
+        box-shadow: 0px 0px 40px lightgray;
     }
 
 </style>
